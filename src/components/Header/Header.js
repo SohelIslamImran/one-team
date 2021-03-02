44d5df175dff89b3f6457da3895eb94e9ffabb01
Sohel Islam Imran <sohelislamimran@gmail.com>
2021-03-02 22:32:11 +0600

    React Practice Project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img src="../../logo.png" alt=""/>
+            <h1>ONE TEAM</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/User/User.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './User.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faVenusMars, faEnvelope, faPhone, faMapMarkedAlt, faMoneyCheckAlt, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+
+const User = (props) => {
+    const { image, firstName, lastName, gender, email, phone, address, salary } = props.user;
+    const [btnDisable, setBtnDisable] = useState(false);
+
+    return (
+        <div className="user">
+            <img src={image} alt=""/>
+            <div>
+                <p><FontAwesomeIcon className="icon" icon={faUser} /> Name: <span className="name">{firstName} {lastName}</span></p>
+                <p><FontAwesomeIcon className="icon" icon={faVenusMars} /> Gender: {gender}</p>
+                <p><FontAwesomeIcon className="icon" icon={faEnvelope} /> Email: {email}</p>
+                <p><FontAwesomeIcon className="icon" icon={faPhone} /> Phone: {phone}</p>
+                <p><FontAwesomeIcon className="icon" icon={faMapMarkedAlt} /> Address: {address}</p>
+                <p><FontAwesomeIcon className="icon" icon={faMoneyCheckAlt} /> Salary: ${salary}</p>
+            </div>
+            <button onClick={() => {
+                        props.addToCart(props.user);
+                        setBtnDisable(true);
+                    }} className={btnDisable ? "add-btn disabled" : "add-btn"}><FontAwesomeIcon className="icon" icon={faPlusSquare} /> {btnDisable ? "Added" : "Add"}</button>
+        </div>
+    );
+};
+
+export default User;
